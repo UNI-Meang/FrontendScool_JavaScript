2bf56267d56e39877ce4644cc936f9d1041a4ed9
UNIcode <82393165+UNI-Meang@users.noreply.github.com>
2021-12-15 13:00:17 +0900

    Calculator for the number of letters in CV

---
+++ calculator.js
@@ -0,0 +1,9 @@
+function counter() {
+    var content = document.getElementById('jasoseol').value;
+    if (content.length > 200) {
+        content = content.substring(0, 200);
+        document.getElementById('jasoseol').value = content;
+    }
+    document.getElementById('count').innerHTML = '('+ content.length + '/200)';
+}
+counter();
